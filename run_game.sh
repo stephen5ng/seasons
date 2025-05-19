ce3da07e652cc77eff11cce3c2d88022d90b821c
Stephen Ng <stephen5.ng@gmail.com>
2025-05-19 13:25:56 -0700

    set speaker volume

--- run_game.sh
+++ run_game.sh
@@ -4,5 +4,6 @@ curl -X POST http://wled-e56890.local/json/settings -H "Content-Type: applicatio
 
 curl -X POST http://wled-f4afec.local/json/settings -H "Content-Type: application/json" -d '{"live":true}'
 
+amixer sset 'Speaker' 90%
 . ./env/bin/activate
 ./seasons.py --led 300
