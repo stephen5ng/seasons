34ef0373150515eca360c18e3758a4dd711864f9
Stephen Ng <stephen5.ng@gmail.com>
2025-05-19 06:39:12 -0700

    run_game.sh

---
+++ run_game.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -X POST http://wled-e56890.local/json/settings \         
+  -H "Content-Type: application/json" \
+  -d '{"live":true}'
+
+curl -X POST http://wled-f4afec.local/json/settings \         
+  -H "Content-Type: application/json" \
+  -d '{"live":true}'
+
+. ./env/bin/activate
+./seasons.py --led 300
